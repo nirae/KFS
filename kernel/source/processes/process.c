ababcdd3517a70cc1122f3750fb9340be8259c50
nirae <hello@nicolasdubouilh.fr>
2021-10-21 19:38:05 +0200

    cleaning the switch task

--- kernel/source/processes/process.c
+++ kernel/source/processes/process.c
@@ -6,7 +6,7 @@
 /*   By: ndubouil <ndubouil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/24 15:40:23 by ndubouil          #+#    #+#             */
-/*   Updated: 2021/10/21 19:31:04 by ndubouil         ###   ########.fr       */
+/*   Updated: 2021/10/21 19:36:27 by ndubouil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,6 +44,11 @@ t_process   *create_process(t_mempage_directory *directory)
 
     return new;
 }
+
+// void        remove_process(t_process *process)
+// {
+//     kfree(process);
+// }
 
 void exit(void)
 {
@@ -153,20 +158,7 @@ void switch_task()
     if (!current_task)
         return;
 
-    // Read esp, ebp now for saving later on.
-    uint32 esp, ebp, eip;
-    asm volatile("mov %%esp, %0" : "=r"(esp));
-    asm volatile("mov %%ebp, %0" : "=r"(ebp));
-
-    // Read the instruction pointer. We do some cunning logic here:
-    // One of two things could have happened when this function exits - 
-    //   (a) We called the function and it returned the EIP as requested.
-    //   (b) We have just switched tasks, and because the saved EIP is essentially
-    //       the instruction after get_eip(), it will seem as if get_eip has just
-    //       returned.
-    // In the second case we need to return immediately. To detect it we put a dummy
-    // value in EAX further down at the end of this function. As C returns values in EAX,
-    // it will look like the return value is this dummy value! (0x12345).
+    uint32 eip;
 
     if (current_task->next)
         future_task = current_task->next;
@@ -180,26 +172,16 @@ void switch_task()
         return;
     }
 
-
-    // Have we just switched tasks?
-    // if (eip == 0x12345)
-    //     return;
-
-
     // No, we didn't switch tasks. Let's save some register values and switch.
     current_task->eip = eip;
-    current_task->esp = esp;
-    current_task->ebp = ebp;
-    // disable_interrupts();
-    // printk("from [%d] ", current_task->pid);
-    // enable_interrupts();
+    GET_ESP(current_task->esp);
+    GET_EBP(current_task->ebp);
+
     // Get the next task to run.
     current_task = current_task->next;
     // If we fell off the end of the linked list start again at the beginning.
-    if (!current_task) current_task = ready_queue;
-    // disable_interrupts();
-    // printk("to [%d]\n", current_task->pid);
-    // enable_interrupts();
+    if (!current_task)
+        current_task = ready_queue;
 
     if (current_task->status == STATUS_DEAD) {
         // enable_interrupts();
@@ -208,16 +190,6 @@ void switch_task()
 
     // Make sure the memory manager knows we've changed page directory.
     current_directory = current_task->page_directory;
-    // Here we:
-    // * Stop interrupts so we don't get interrupted.
-    // * Temporarily puts the new EIP location in ECX.
-    // * Loads the stack and base pointers from the new task struct.
-    // * Changes page directory to the physical address (physicalAddr) of the new directory.
-    // * Puts a dummy value (0x12345) in EAX so that above we can recognise that we've just
-    //   switched task.
-    // * Restarts interrupts. The STI instruction has a delay - it doesn't take effect until after
-    //   the next instruction.
-    // * Jumps to the location in ECX (remember we put the new EIP in there).
     // printk("switch on %d eip[%p], esp[%p], ebp[%p] dir [%p]\n", current_task->pid, eip, esp, ebp, current_directory->physical_address);
     perform_task_switch(current_task->eip, current_directory->physical_address, current_task->ebp, current_task->esp);
     // enable_interrupts();
